1532f14feed2c05cd833c2410895249d05802647
eslam <eslamewada63@gmail.com>
2025-03-19 01:35:24 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// To change the appearance of the phone
+
+let img = document.querySelector(".img");
+function phone(x) {
+    img.src = x;
+}
+
+// To change the background shape
+let container = document.querySelector(".container")
+function bg(bg) {
+    container.style.backgroundImage = bg;
+};
