ac30872bd3cd4395aa8503acc350f69c3d040e93
Eslam <https://github.com/Eslamhossam2024>
2026-03-15 03:42:24 +0200

    updatenn

--- js/main.js
+++ js/main.js
@@ -10,3 +10,20 @@ let container = document.querySelector(".container")
 function bg(bg) {
     container.style.backgroundImage = bg;
 };
+
+// scroll ..
+let up = document.getElementById('up');
+window.onscroll = function(){
+    if(scrollY >= 20){
+        up.style.display = "block"
+    }else{
+        up.style.display = "none"
+    }
+}
+up.onclick = function(){
+    scroll({
+        top:0,
+        left:0,
+        behavior:'smooth'
+    })
+};
